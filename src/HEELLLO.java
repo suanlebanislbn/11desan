7d4b7d1a1589e5604ffdfbcf72d5a6c9f6a484dc
suanlebanislbn <1779622883@qq.com>
2018-06-30 11:35:00 +0800

    HEELLLO.java 添加函数toString

--- src/HEELLLO.java
+++ src/HEELLLO.java
@@ -2,4 +2,10 @@ public class HEELLLO {
     public void fun(int a){
         System.out.print(a*a*a*a);
     }
+
+    @Override
+    public String toString() {
+        System.out.print("红莲骑士兽");
+       return "战斗暴龙兽";
+    }
 }
